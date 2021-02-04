9373f99d8ad9cb7c422ae6567ef80702da6b4863
David Lobo <david@davidlobo.co.uk>
2021-02-04 01:22:23 +0000

    Replace reset password url with SPA url

--- laravel-sanctum/routes/web.php
+++ laravel-sanctum/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\Auth\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/reset-password/{token}', ResetPasswordController::class . '@index')
+    ->middleware('guest')->name('password.reset');
